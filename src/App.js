8be7b14e3931adb80f056c64c3c7ad7487dbf5a4
gosoohyeon <kosh1013@gmail.com>
2022-12-23 16:56:59 +0900

    sass환경 추가, 기본 layout세팅

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import Header from './components/layout/header';
+import Footer from './components/layout/footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/layout/header.js
@@ -0,0 +1,12 @@
+import Category from './menu_category'
+
+function Header() {
+  return (
+    <header>
+        <Category></Category>
+        header
+    </header>
+  );
+}
+
+export default Header;
